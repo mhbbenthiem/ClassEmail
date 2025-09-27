0d955b9770dc83ab658149864b3d5bbfa5727f9c
Helobenthiem <maria.benthiem@pinho.com.br>
2025-09-27 18:04:43 -0300

    att

--- api/index.py
+++ api/index.py
@@ -1,9 +1,7 @@
 # api/index.py
 import sys
 from pathlib import Path
-
 ROOT = Path(__file__).resolve().parent.parent
 if str(ROOT) not in sys.path:
     sys.path.insert(0, str(ROOT))
-
-from backend.app import app  # precisa existir uma variável 'app'
+from backend.app import app
